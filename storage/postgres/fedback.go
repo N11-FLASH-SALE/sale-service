2ac9e6aa6a9ee1d0332cb0c6a9d0735f7a7e46a4
Firdavs G'ayratov <gayratovfirdavs720@gmail.com>
2024-08-29 02:51:16 +0500

    add feedback and bought

--- storage/postgres/fedback.go
+++ storage/postgres/fedback.go
@@ -1,4 +1,30 @@
 package postgres
 
+import (
+	"database/sql"
+	"log/slog"
+	logger "sale/logs"
+	pb "sale/genproto/sale"
 
-func 
+)
+
+type FeedbackRepository struct {
+	Db *sql.DB
+	lg *slog.Logger
+}
+
+func NewFeedbackRepository(db *sql.DB) *FeedbackRepository {
+	return &FeedbackRepository{Db: db, lg: logger.NewLogger()}
+}
+
+func (repo *FeedbackRepository) CreateFeedback(request *pb.CreateFeedbackRequest) (*pb.FeedbackResponse, error) {
+	return nil, nil
+}
+
+func (repo *FeedbackRepository) GetFeedback(request *pb.GetFeedbackRequest) (*pb.GetFeedbackResponse, error) {
+	return nil, nil
+}
+
+func (repo *FeedbackRepository) GetFeedbackOfUser(request *pb.GetFeedbackOfUserRequest) (*pb.GetFeedbackOfUserResponse, error) {
+	return nil, nil
+}
