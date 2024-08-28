d13498f7a4e3082724050dc69bedf9425027b7f7
nurmuhammad701 <nurmuhammadmel@gmail.com>
2024-08-28 15:35:02 +0500

    all

--- storage/mongosh/product.go
+++ storage/mongosh/product.go
@@ -19,14 +19,15 @@ func NewProductsRepository(db *mongo.Database) *ProductsRepo {
 
 func (r *ProductsRepo) CreateProduct(ctx context.Context, req *pb.CreateProductRequest) (*pb.ProductId, error) {
 
-	price := req.Price - req.Price*(float64(req.Stock)/100)
+	stockPercentage := float64(req.Stock) / 100
+	priceWithStock := req.Price - req.Price*stockPercentage
 
 	product := bson.D{
 		{Key: "name", Value: req.Name},
 		{Key: "description", Value: req.Description},
 		{Key: "price", Value: req.Price},
 		{Key: "stock", Value: req.Stock},
-		{Key: "price_with_stock", Value: price},
+		{Key: "price_with_stock", Value: priceWithStock},
 		{Key: "limit_of_product", Value: req.LimitOfProduct},
 		{Key: "size", Value: req.Size},
 		{Key: "color", Value: req.Color},
@@ -42,3 +43,7 @@ func (r *ProductsRepo) CreateProduct(ctx context.Context, req *pb.CreateProductR
 	id := result.InsertedID.(primitive.ObjectID).Hex()
 	return &pb.ProductId{Id: id}, nil
 }
+
+// func (r *ProductsRepo) GetProduct(ctx context.Context, id string) (*pb.GetProductResponse, error) {
+
+// }
