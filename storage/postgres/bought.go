40adbb368d39777c5103fbbc6ddd6385147327b7
Firdavs G'ayratov <gayratovfirdavs720@gmail.com>
2024-08-29 02:52:22 +0500

    add bougth

--- storage/postgres/bought.go
+++ storage/postgres/bought.go
@@ -1,5 +1,30 @@
 package postgres
 
+import (
+	"context"
+	"database/sql"
+	"log/slog"
+	logger "sale/logs"
+	pb "sale/genproto/sale"
+)
 
+type BougthRepository struct {
+	Db *sql.DB
+	lg *slog.Logger
+}
 
-func
+func NewBougthRepository(db *sql.DB) *BougthRepository {
+	return &BougthRepository{Db: db, lg: logger.NewLogger()}
+}
+
+func (repo *BougthRepository) CreateBought(ctx context.Context, req *pb.CreateBoughtRequest) (*pb.BoughtResponse, error){
+	return nil, nil
+}
+
+func (repo *BougthRepository) GetBought(ctx context.Context, req *pb.GetBoughtRequest) (*pb.GetBoughtResponse, error){
+	return nil, nil
+}
+
+func (repo *BougthRepository) GetBoughtOfUser(ctx context.Context, req *pb.GetBoughtOfUserRequest) (*pb.GetBoughtOfUserResponse, error){
+	return nil, nil
+}
