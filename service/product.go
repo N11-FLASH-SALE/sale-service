a9f75ed69993a79582c06b5abe707f83f95a8cff
nurmuhammad701 <nurmuhammadmel@gmail.com>
2024-08-29 12:53:56 +0500

    All

--- service/product.go
+++ service/product.go
@@ -1,20 +1,32 @@
 package service
 
 import (
+	"context"
 	"log/slog"
 	pb "sale/genproto/sale"
 	"sale/storage"
 )
 
-type HealthMonitoringService struct {
+type ProductService struct {
 	pb.UnimplementedProductServer
 	logger *slog.Logger
 	repo   storage.Istorage
 }
 
-func NewHealthMonitoringService(logger *slog.Logger, repo storage.Istorage) *HealthMonitoringService {
-	return &HealthMonitoringService{
+func NewProductService(logger *slog.Logger, repo storage.Istorage) *ProductService {
+	return &ProductService{
 		logger: logger,
 		repo:   repo,
 	}
+}
+
+func (s ProductService) CreateProduct(ctx context.Context, req *pb.CreateProductRequest) (*pb.ProductId, error) {
+	s.logger.Info("CreateProduct rpc method is started")
+	res, err := s.repo.Product().CreateProduct(ctx, req)
+	if err != nil {
+		s.logger.Error(err.Error())
+		return nil, err
+	}
+	s.logger.Info("CreateProduct finished succesfully")
+	return res, nil
 }
